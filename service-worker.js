7d2ba552505450facf6eaecf4669e4021b9ccb86
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2024-04-10 16:55:24 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,22 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open('pwa-cache').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/styles.css',
         '/script.js',
-        '/favicon-192x192.png',
-        '/favicon-512x512.png'
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
